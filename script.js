f2d3c9c2e7607595fd258d9cc03e5b73177e65ef
GuilhermeVeigaa <guilhermeveigafff@gmail.com>
2023-06-28 22:46:25 -0300

    upload do projeto

---
+++ script.js
@@ -0,0 +1,175 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const porcentoButton = document.querySelector("[data-operator-porcento]");
+const quadradoButton = document.querySelector("[data-operator-quadrado]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector(
+    "[data-previous-operand]"
+);
+const currentOperandTextElement = document.querySelector(
+    "[data-current-operand]"
+);
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    formatDisplayNumber(number) {
+        const stringNumber = number.toString();
+    
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+    
+        let integerDisplay;
+    
+        if (isNaN(integerDigits)) {
+          integerDisplay = "";
+        } else {
+          integerDisplay = integerDigits.toLocaleString("en", {
+            maximumFractionDigits: 0,
+          });
+        }
+    
+        if (decimalDigits != null) {
+          return `${integerDisplay}.${decimalDigits}`;
+        } else {
+          return integerDisplay;
+        }
+      }
+
+    quadrado() {
+        const _currentOperand = parseFloat(this.currentOperand);
+
+        let calc = _currentOperand * _currentOperand;
+        let resultado = calc;
+
+        this.currentOperand = resultado;
+        this.operation = undefined; 
+        this.previousOperand = "";
+    }
+    porcento() {
+        const _currentOperand = parseFloat(this.currentOperand);
+
+        const calc = _currentOperand / 100;
+        let resultado = calc;
+
+        this.currentOperand = resultado;
+        this.operation = undefined; 
+        this.previousOperand = "";
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    calculate() {
+        let result;
+        
+        const _previousOperand = parseFloat(this.previousOperand);
+        const _currentOperand = parseFloat(this.currentOperand);
+
+        if (isNaN(_previousOperand) || isNaN(_currentOperand)) return;
+
+        switch (this.operation) {
+            case "+":
+                result = _previousOperand + _currentOperand;
+                break;
+            case "−":
+                result = _previousOperand - _currentOperand;
+                break;
+            case "÷":
+                result = _previousOperand / _currentOperand;
+                break;
+            case  "×":
+                result = _previousOperand * _currentOperand;
+                break;  
+            default:
+                return;           
+        }
+
+      this.currentOperand = result;
+      this.operation = undefined;
+      this.previousOperand = "";
+    }
+
+    chooseOperation(operation) {
+        if (this.previousOperand !== "") {
+            this.calculate()
+        }
+
+        this.operation = operation;
+
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    } 
+
+    appendNumber(number) {
+        if (this.currentOperand.includes(".") && number === ".") return;
+
+        this.currentOperand = `${this.currentOperand}${number.toString()}`;
+    }
+
+    clear() {
+        this.currentOperand = "0";
+        this.previousOperand = " ";
+        this.operation = undefined;
+    }
+
+    updateDisplay() {
+        this.previousOperandTextElement.innerText = `${this.formatDisplayNumber(
+          this.previousOperand
+        )} ${this.operation || ""}`;
+        this.currentOperandTextElement.innerText = this.formatDisplayNumber(
+          this.currentOperand
+        );
+      }
+    }
+
+const calculator = new Calculator (
+    previousOperandTextElement,
+    currentOperandTextElement
+);
+
+for (const numberButton of numberButtons) {
+    numberButton.addEventListener("click", () => {
+        calculator.appendNumber(numberButton.innerText);
+        calculator.updateDisplay();
+    });
+}
+
+for (const operationButton of operationButtons) {
+    operationButton.addEventListener("click", () => {
+        calculator.chooseOperation(operationButton.innerText);
+        calculator.updateDisplay();
+    })
+}
+
+allClearButton.addEventListener("click", () => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+equalsButton.addEventListener("click", () =>  {
+    calculator.calculate();
+    calculator.updateDisplay();
+});
+
+quadradoButton.addEventListener("click", () => {
+    calculator.quadrado();
+    calculator.updateDisplay();
+});
+
+porcentoButton.addEventListener("click", () => {
+    calculator.porcento();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener("click", () => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
